21c313be57db2ab686b20a698308a813bcd81d76
fhoward123 <42585731+fhoward123@users.noreply.github.com>
2019-03-11 16:12:10 -0500

    Created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,18 +2,29 @@ console.log("Up and running!");
 
 let cards = [ "queen", "queen", "king", "king" ];
 let cardsInPlay = [];
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardsInPlay[cardsInPlay.length - 1] );
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[cardsInPlay.length - 1] );
 
-if ( cardsInPlay.length === 2 ) {
-	if ( cardsInPlay[0] === cardsInPlay[1] ) {
-		alert("You found amatch!");
-	}
-	else {
-		alert("Sorry, try again.");
+let checkForMatch = () => {
+	if ( cardsInPlay.length === 2 ) {
+		if ( cardsInPlay[0] === cardsInPlay[1] ) {
+			alert("You found amatch!");
+		}
+		else {
+			alert("Sorry, try again.");
+		}
 	}
 }
+
+let flipCard = (cardId) => {
+	// let cardOne = cards[0];
+	// cardsInPlay.push(cardOne);
+	// console.log("User flipped " + cardsInPlay[cardsInPlay.length - 1] );
+	// let cardTwo = cards[2];
+	// cardsInPlay.push(cardTwo);
+	console.log("User flipped " + cards[cardId] );
+	cardsInPlay.push(cardId);
+	checkForMatch();
+	
+}
+flipCard(0);
+flipCard(2);
+
